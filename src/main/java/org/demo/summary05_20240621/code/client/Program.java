79b56e86ee25d863c0af7164ba6567ad4fda6ec0
USER <jolivuk@gmail.com>
2024-06-24 10:42:29 +0200

    comment added

--- src/main/java/org/demo/summary05_20240621/code/client/Program.java
+++ src/main/java/org/demo/summary05_20240621/code/client/Program.java
@@ -2,6 +2,7 @@ package org.demo.summary05_20240621.code.client;
 
 public class Program {
     public static void main(String[] args) {
+        // запуск приложения
         App.ButtonClick();
     }
 }
